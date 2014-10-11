554aaf3c051280a85ad7800cef5aba18ff55b683
jechavarriah <negrotico19@gmail.com>
2014-10-11 00:10:12 -0600

    inicio reporte 7

--- laboratorio7/principal_vector.cpp
+++ laboratorio7/principal_vector.cpp
@@ -2,8 +2,13 @@
 
 int main(void){
 	int x[] = {1,2,3};
-	//int N = size(x);
-	//int N = 3;
+	int y[] = {1,1,1};
 	vector<int,3> V1(x);                                     
+	vector<int,3> V2(y);
+	vector<int,3> V3(y);
+	vector<int,3> V4(y);
+	//V3=V3.suma(&V1,&V2);
+	//V4=V4.resta(&V1,&V2);
+	//double norma_V2 = V2.norma(&V2);
     return 0;
 }
